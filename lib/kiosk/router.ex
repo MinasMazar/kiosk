3ed2795646525fb5c5dd535df4d676b2f7cc13eb
MinasMazar <5578468+MinasMazar@users.noreply.github.com>
2024-10-08 20:47:13 +0200

    Redirect root to kiosk

--- lib/kiosk/router.ex
+++ lib/kiosk/router.ex
@@ -6,12 +6,22 @@ defmodule Kiosk.Router do
   plug :dispatch
 
   get "/" do
+    conn
+    |> put_resp_header("Location", "/kiosk")
+    |> send_resp(302, "/kiosk")
+  end
+
+  get "/kiosk" do
     {status, result} =
       case Kiosk.display(conn.body_params) do
 	{:ok, result} -> {200, result}
 	{:error, reason} -> {400, reason}
       end
     send_resp(conn, status, result)
+  end
+
+  get "/dev" do
+    send_resp(conn, 200, "This is a dev page.")
   end
 
   match _ do
