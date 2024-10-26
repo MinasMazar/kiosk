2b65a2dd46ea3b52b6961c35f755c5c3dc32ea7e
MinasMazar <5578468+MinasMazar@users.noreply.github.com>
2024-10-26 17:07:50 +0200

    Fix timeout handling (in server, not browser)

--- lib/kiosk/browser.ex
+++ lib/kiosk/browser.ex
@@ -1,5 +1,4 @@
 defmodule Kiosk.Browser do
-  @reload_timeout 20 * 60 * 1000
   use GenServer
 
   def open(url: url) do
@@ -16,32 +15,21 @@ defmodule Kiosk.Browser do
 
   def init(_) do
     {:ok,
-     %{url: "http://localhost:4000/", reload_timeout: @reload_timeout}
-     |> schedule_start()
-     |> schedule_reload()}
+     %{url: "http://localhost:4000/"}
+     |> schedule_start()}
   end
 
   def state() do
     GenServer.call(__MODULE__, :state)
   end
 
-  def alter_state(overrides) do
-    GenServer.cast(__MODULE__, {:state, overrides})
-  end
-
   def handle_call(:state, _, state), do: {:reply, state, state}
-  def handle_cast({:state, overrides}, state), do: {:noreply, Map.merge(state, overrides)}
 
   def handle_info(:start, state) do
     IO.puts("Starting browser..")
     port = Port.open({:spawn, ~s[midori -e fullscreen "#{state.url}"]}, [])
     ref = Port.monitor(port)
     {:noreply, Map.put(state, :port, {port, ref})}
-  end
-
-  def handle_info(:reload, state) do
-    IO.puts("Reloading browser page..")
-    reload() && {:noreply, schedule_reload(state)}
   end
 
   def handle_info({_port, {:data, _message}}, state) do
@@ -54,9 +42,5 @@ defmodule Kiosk.Browser do
 
   defp schedule_start(state) do
     Process.send_after(self(), :start, 4_000) && state
-  end
-
-  defp schedule_reload(state) do
-    Process.send_after(self(), :reload, state.reload_timeout) && state
   end
 end
--- lib/kiosk/server.ex
+++ lib/kiosk/server.ex
@@ -1,4 +1,5 @@
 defmodule Kiosk.Server do
+  @default_timeout 6 * 60
   use GenServer
 
   def start_link(args) do
@@ -7,7 +8,11 @@ defmodule Kiosk.Server do
 
   def init(_) do
     Kiosk.set_page(:kiosk)
-    {:ok, schedule_next([])}
+    {:ok, schedule_next(%{timeout: @default_timeout})}
+  end
+
+  def set_timeout(pid \\ __MODULE__, timeout) do
+    GenServer.call(pid, {:state, %{timeout: timeout}})
   end
 
   def handle_info(:run, state) do  
@@ -16,8 +21,14 @@ defmodule Kiosk.Server do
     {:noreply, schedule_next(state)}
   end
 
+  def handle_call({:state, overrides}, _, state) do
+    with state <- Map.merge(state, overrides) do
+      {:reply, state, state}
+    end
+  end
+
   def schedule_next(state) do
-    Process.send_after(self(), :run, 30_000)
+    Process.send_after(self(), :run, state.timeout * 1000)
     state
   end
 end
